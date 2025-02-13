91eb7d5bb78384cc5e41a8742985a0a301505e93
Nathan Woolf <nathanwoolf38@gmail.com>
2025-02-13 16:20:37 -0600

    annotations

--- Lab1-SPART/driver.v
+++ Lab1-SPART/driver.v
@@ -29,5 +29,16 @@ module driver(
     inout [7:0] databus
     );
 
+//  DIP Setting to Baud Rate mapping (comes in through I/O address)
+//  00 -> 4800  ->  650 -> 0x28A
+//  01 -> 9600  ->  325 -> 0x145
+//  10 -> 19200 ->  162 -> 0xA2
+//  11 -> 38400 ->  80  -> 0x50
+
+//  IOADDR bit mappings
+//  00 -> Tx buffer (R/W = 0) : Rx buffer (R/W = 1)
+//  01 -> Status register (R/W = 1)
+//  10 -> DB (low) division buffer
+//  11 -> DB (high) division buffer
 
 endmodule
--- Lab1-SPART/spart.v
+++ Lab1-SPART/spart.v
@@ -30,5 +30,25 @@ module spart(
     input rxd
     );
 
+//  creating baud rate generator
+
+//  DIP Setting to Baud Rate mapping (comes in through I/O address)
+//  00 -> 4800  ->  650 -> 0x28A
+//  01 -> 9600  ->  325 -> 0x145
+//  10 -> 19200 ->  162 -> 0xA2
+//  11 -> 38400 ->  80  -> 0x50
+
+//  IOADDR bit mappings
+//  00 -> Tx buffer (R/W = 0) : Rx buffer (R/W = 1)
+//  01 -> Status register (R/W = 1)
+//  10 -> DB (low) division buffer
+//  11 -> DB (high) division buffer
+
+reg [15:0] div_buf;
+always_ff @(posedge clk, posedge rst) begin 
+    if (rst) begin 
+        div_buf <= 16'0b;
+    end
+end
 
 endmodule
